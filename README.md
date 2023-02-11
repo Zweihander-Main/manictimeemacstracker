498e03a77c2a9fdd405dfae4d0f1734b85d38f9f
Zweihänder <zweidev@zweihander.me>
2023-02-11 14:51:16 -0500

    feat(root): add in old manictime plugin

---
+++ README.md
@@ -0,0 +1,5 @@
+# Manic Time plugin for Emacs
+
+> Tracks Emacs usage for ManicTime (Windows)
+
+A quick experiment which seemed to work at the time however I no longer use ManicTime.
---
+++ manictime-client-plugin-example/source/tracker-plugin/Plugins.Emacs/EmacsFileRetreiver.cs
@@ -0,0 +1,48 @@
+﻿using Finkit.ManicTime.Shared.DocumentTracking;
+using ManicTime.Client.Tracker.EventTracking.Publishers.ApplicationTracking;
+
+namespace Plugins.Emacs
+{
+    public class EmacsFileRetreiver : IDocumentRetreiver
+    {
+        public DocumentInfo GetDocument(ApplicationInfo application)
+        {
+            // first we use processName to see if it is Emacs. If it is not Emacs, we just return null and tracker will move on to the next plugin
+
+            if (!CheckForProcess(application))
+                return null;
+
+            // get the filename
+            string filename = GetFilename(application);
+
+            // DocumentName - this will be displayed in the details view (bottom left part of ManicTime)
+            // DocumentGroupName = this will be displayed on the bottom right side (group name)
+            // DocumentType = file, url...
+
+
+            // Emacs will only display filename, not the full path, so we will use the same filename for DocumentName and DocumentGroupName
+            // if we could get the full path, for example c:\dir\filename.txt, then we could use 
+            // 
+            // DocumentName = c:\dir\filename.txt
+            // DocumentGroupName = filename.txt
+
+            if (filename != null)
+                return new DocumentInfo() { DocumentName = filename, DocumentGroupName = filename, DocumentType = DocumentTypes.File };
+
+            return null;
+        }
+
+        private bool CheckForProcess(ApplicationInfo application)
+        {
+            if (application.ProcessName == "emacs")
+                return true;
+
+            return false;
+        }
+
+        private string GetFilename(ApplicationInfo application)
+        {
+            return application.WindowTitle.Trim();
+        }
+    }
+}
